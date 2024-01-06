3d2ca1f29c814bfc49336fe56aeba8c2743e580f
Aniket Chitransh <ani.chitransh@gmail.com>
2024-01-06 11:19:58 +0530

    handleSubmit and handleChange functions code written. Feature add to send message to the text area

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,14 +1,25 @@
 import React, { useState } from 'react'
+import { sendMessage, isTyping } from 'react-chat-engine';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 
-const MessageForm = () => {
+const MessageForm = (props) => {
     const [value, setValue] = useState('');
+    const { chatId, creds } = props;
 
-    const handleSubmit = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
+        const text = value.trim();
+
+        if (text.length > 0) sendMessage(creds, chatId, { text });
+
+        setValue('');
     }
 
-    const handleChange = () => {
+    const handleChange = (event) => {
+        setValue(event.target.value);
 
+        isTyping(props, chatId);
     }
 
     return (
@@ -20,6 +31,11 @@ const MessageForm = () => {
                 onChange={handleChange}
                 onSubmit={handleSubmit}
             />
+            <label htmlFor="upload-button">
+                <span className="image-button">
+
+                </span>
+            </label>
         </form>
     )
 }

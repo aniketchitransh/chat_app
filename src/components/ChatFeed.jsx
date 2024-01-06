b433ff3418963da87ff7a427561cab440ef7fc7b
Aniket Chitransh <ani.chitransh@gmail.com>
2024-01-06 11:52:12 +0530

    Going to create login form

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -7,6 +7,19 @@ const ChatFeed = (props) => {
 
     const chat = chats && chats[activeChat];
 
+    const renderReadReceipts = (message, isMyMessage) => {
+        chat.people.map((person, index) => person.last_read == message.id && (
+            <div
+                key={`read_${index}`}
+                className="read-receipt"
+                style={{
+                    float: isMyMessage ? 'right' : 'left',
+                    backgroundImage: person.person.avatar && `url(${person.person.avatar})`
+                }}
+            />
+        ))
+    }
+
     const renderMessages = () => {
         const keys = Object.keys(messages);
 
@@ -33,7 +46,7 @@ const ChatFeed = (props) => {
                             }
                         }
                     >
-                        read-receipts
+                        {renderReadReceipts(message, isMyMessage)}
                     </div>
                 </div>
             )

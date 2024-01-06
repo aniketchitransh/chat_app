d814a6db4422a319dce41577e4da7ef03a89f121
Aniket Chitransh <ani.chitransh@gmail.com>
2024-01-06 08:23:00 +0530

    Created two users from ChatEngine dashboard

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
 import { ChatEngine } from "react-chat-engine";
+import ChatFeed from './components/ChatFeed';
 
 import './App.css'
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="be00d52e-63f2-4433-a801-2603396021f9"
+            userName="aniket"
+            userSecret="password"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
+        />
+    );
+};
+
+export default App;
